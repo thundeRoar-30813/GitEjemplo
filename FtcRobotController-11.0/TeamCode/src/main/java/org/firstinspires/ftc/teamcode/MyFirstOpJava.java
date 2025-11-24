be4367a1314af21a0362498d2dba3f0ed3ce323c
Daniel Echeverria <alejandro.garciae@uanl.edu.mx>
2025-11-23 20:12:48 -0600

    Agregando la carpeta de programacion del robot

---
+++ FtcRobotController-11.0/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MyFirstOpJava.java
@@ -0,0 +1,45 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DigitalChannel;
+import com.qualcomm.robotcore.hardware.DistanceSensor;
+import com.qualcomm.robotcore.hardware.Gyroscope;
+import com.qualcomm.robotcore.hardware.Servo;
+
+@TeleOp
+public class MyFirstOpJava extends LinearOpMode {
+    private Gyroscope imu;
+    private DcMotor motorTest;
+    private DigitalChannel digitalTouch;
+    private DistanceSensor sensorColorRange;
+    private Servo servoTest;
+
+
+    @Override
+    public void runOpMode() {
+
+        motorTest = hardwareMap.get(DcMotor.class, "motor1");
+
+
+        telemetry.addData("Status", "Initialized");
+        telemetry.update();
+        // Wait for the game to start (driver presses PLAY)
+        waitForStart();
+
+        // run until the end of the match (driver presses STOP)
+        // run until the end of the match (driver presses STOP)
+        double tgtPower = 0;
+        while (opModeIsActive()) {
+            tgtPower = -this.gamepad1.left_stick_y;
+            motorTest.setPower(tgtPower);
+            telemetry.addData("Target Power", tgtPower);
+            telemetry.addData("Motor Power", motorTest.getPower());
+            telemetry.addData("Status", "Running");
+            telemetry.update();
+
+        }
+    }
+
+}
